3e6e36085d54ebc2cb8023c3bbd487d6b72f6b5d
Zen Rydge <zukwaz@proton.me>
2025-07-06 10:30:57 +0530

    Namespace class added.

---
+++ Zukwaz.CSharp.MvvmGenerator/Namespace/Namespace.cs
@@ -0,0 +1,8 @@
+﻿namespace Zukwaz.CSharp.MvvmGenerator
+{
+    public sealed class Namespace
+    {
+        public string Name { set; get; } = string.Empty;
+        public List<Class> Classes { get; } = new List<Class>();
+    }
+}
