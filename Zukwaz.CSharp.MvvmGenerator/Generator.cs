7191668d6469f41127b21ecf92c19f5e1e19f475
Zen Rydge <zukwaz@proton.me>
2025-07-06 10:34:42 +0530

    Generator added.

---
+++ Zukwaz.CSharp.MvvmGenerator/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Zukwaz.CSharp.MvvmGenerator
+{
+    public abstract class Generator
+    {
+        protected const char DQ = '\"'; // Double Quotes
+
+        protected static string Tab(int total)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < total; i++)
+            {
+                builder.Append($@"    ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
